4b74033401f29840f942980d8fe20e70b8a3b914
unknown <kubawarzyn@gmail.com>
2020-05-20 21:34:28 +0200

    revert

--- run.py
+++ run.py
@@ -18,8 +18,8 @@ os.system("python license-plate-ocr.py %s)" % (output_dir))
 os.system("python gen-outputs.py %s %s > %s)" % (input_dir, output_dir, csv_file))
 
 # Clean files and draw output
-# os.remove("%s/*_lp.png" % (output_dir))
-# os.remove("%s/*car.png" % (output_dir))
-# os.remove("%s/*_cars.txt" % (output_dir))
-# os.remove("%s/*_lp.txt" % (output_dir))
-# os.remove("%s/*_str.txt" % (output_dir))
+os.remove("%s/*_lp.png" % (output_dir))
+os.remove("%s/*car.png" % (output_dir))
+os.remove("%s/*_cars.txt" % (output_dir))
+os.remove("%s/*_lp.txt" % (output_dir))
+os.remove("%s/*_str.txt" % (output_dir))
