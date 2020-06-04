e9581222eae2bac18b5f424dd432cc86387a27ae
unknown <kubawarzyn@gmail.com>
2020-06-04 21:32:26 +0200

    asdas

--- license_plate_ocr.py
+++ license_plate_ocr.py
@@ -11,7 +11,9 @@ from src.label import dknet_label_conversion
 from src.utils import nms
 
 def ocr(img, ocr_net, ocr_meta, ocr_threshold):
+	print('OCR detecting...')
 	detected, (width, height) = detect_on_image(ocr_net, ocr_meta, img, thresh=ocr_threshold, nms=None)
+	print('OCR detected')
 	if len(detected):
 		L = dknet_label_conversion(detected, width, height)
 		L = nms(L,.45)
