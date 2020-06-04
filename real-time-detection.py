2bd658b0d3ed440cab0c12eb8b6e884c03012ed8
sstelter <stelter97@gmail.com>
2020-06-04 22:28:26 +0200

    Add 3rd thread to display image

--- real-time-detection.py
+++ real-time-detection.py
@@ -9,8 +9,12 @@ import threading
 import concurrent.futures
 import time
 
-QUEUE_SIZE = 10
+try:
+    import queue
+except ImportError:
+    import Queue as queue
 
+QUEUE_SIZE = 10
 
 def produce_frame(video_stream, img_queue, end_event):
     """
@@ -24,24 +28,33 @@ def produce_frame(video_stream, img_queue, end_event):
         time.sleep(0.01)
 
 
-def consume_frame(img_queue, end_event):
+def consume_frame(img_queue, display_queue, end_event):
     """
     @summary: Run plate detection on first image from provided queue
     """
     while not end_event.is_set():
         img = img_queue.get()
         # do sth with this image :)
-        print('Working on: {}'.format(img))
+        # print('Working on: {}'.format(img))
         time.sleep(0.5) # omit some cases - we cannot process as fast as we rcv
+        display_queue.put(img)  # save to display
 
+
+def display_frame(display_queue, end_event):
+    # TODO add display logic
+    frame = display_queue.get()
+    print(frame)
+    
 
 img_queue = DiscardQueue(QUEUE_SIZE)
+display_queue = queue.Queue()
 end_event = threading.Event()
 vs = WebcamVideoStream(src=0).start()
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
     executor.submit(produce_frame, vs, img_queue, end_event)
-    executor.submit(consume_frame, img_queue, end_event)
+    executor.submit(consume_frame, img_queue, display_queue, end_event)
+    executor.submit(display_frame, display_queue, end_event)
 
 time.sleep(10)
 end_event.set() # finish
