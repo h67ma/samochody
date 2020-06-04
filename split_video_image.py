c6168596261c0eb2d9894b13be2feb6bb4e3c547
unknown <kubawarzyn@gmail.com>
2020-06-04 21:44:59 +0200

    asdqwd

--- split_video_image.py
+++ split_video_image.py
@@ -143,7 +143,7 @@ def main():
         for i, car_img in enumerate(Icars):
             # LPD
             bname = "%s_%dcar" % (file_name, i)
-            print('\t Processing %s' % bname))
+            print('\t Processing %s' % bname)
             lp_img, txt, ok = license_detection(car_img, wpod_net, lp_threshold)
             if not ok:
                 print('not ok')
